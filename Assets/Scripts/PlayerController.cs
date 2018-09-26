c5d3e64b44bb898c33e44192229229a125f9d00b
gadeuneo <8356171+gadeuneo@users.noreply.github.com>
2018-09-26 13:27:27 +0200

    Basic Player Fire
    Added basic player projectile mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+
+    // Player movement speed
     public float playerSpeed;
+    // Player projectile
+    public GameObject projectile;
+    // Spawn of projectile
+    public Transform projectileSpawn;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,5 +21,15 @@ public class PlayerController : MonoBehaviour {
         var horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
         var vertical = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
         transform.Translate(horizontal, vertical, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Instantiate(projectile, transform.position + Vector3.up, Quaternion.identity);
+
     }
 }
---
+++ Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour {
+
+    // Speed of projectile
+    public float projectileSpeed;
+    // Lifetime of projectile
+    public float time;
+    // Direction of projectile
+    public Vector2 direction = new Vector2(0, 1);
+
+    // Use this for initialization
+    void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += new Vector3(direction.x, direction.y, 0) * projectileSpeed * Time.deltaTime;
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
