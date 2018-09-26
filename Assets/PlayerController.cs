b2d97f0bc2b8627d36d88ef9b5a1b3b9cf7ffa7b
BWeinberg77 <43477452+BWeinberg77@users.noreply.github.com>
2018-09-26 12:02:06 +0200

    Basic controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float playerSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        var vertical = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+        transform.Translate(horizontal, vertical, 0);
+    }
+}
