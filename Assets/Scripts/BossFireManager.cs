1b46542dccce14816dbb5bace9878c01c1a36f7b
BWeinberg77 <43477452+BWeinberg77@users.noreply.github.com>
2018-09-30 17:24:23 +0200

    Randomly Chooses Between Two Firing Strategies

--- Assets/Scripts/BossFireManager.cs
+++ Assets/Scripts/BossFireManager.cs
@@ -1,20 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //in progress
 public class BossFireManager : MonoBehaviour {
     public BossProjectileStandard standardBullet;
     public float singleFireSpeed, pulseSpeed; //seconds between single fire bullets
+
+    private Action[] strategies;
+    System.Random rand = new System.Random();
     // Use this for initialization
     void Start () {
+        strategies = new Action[] { slowPulseFastSingle, fastPulseSlowSingle};
+        int i = rand.Next(strategies.Length);
+        strategies[i]();
+
+
         //StartCoroutine(singleFireRoutine(singleFireSpeed, 10));
-        StartCoroutine(pulseRoutine(pulseSpeed, 2, 30));
+        //StartCoroutine(pulseRoutine(pulseSpeed, 2, 30));
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    //Strategies
+    public void slowPulseFastSingle() {
+        StartCoroutine(singleFireRoutine(.2f, 10));
+        StartCoroutine(pulseRoutine(5, 3, 8));
+    }
+
+    public void fastPulseSlowSingle() {
+        StartCoroutine(singleFireRoutine(2, 5));
+        StartCoroutine(pulseRoutine(1, 6, 12));
+    }
+
+    //Fire Patterns
 
     public IEnumerator singleFireRoutine(float firerate, float bulletspeed) {
         standardBullet.projectileSpeed = bulletspeed;
