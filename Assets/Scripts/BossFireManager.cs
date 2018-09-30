36241a9f455f0deecf2d376b5e3a4a5d0611ac86
BWeinberg77 <43477452+BWeinberg77@users.noreply.github.com>
2018-09-30 15:00:52 +0200

    Pulse attack implemented

--- Assets/Scripts/BossFireManager.cs
+++ Assets/Scripts/BossFireManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 //in progress
 public class BossFireManager : MonoBehaviour {
     public BossProjectileStandard standardBullet;
-    public float singleFireSpeed; //seconds between single fire bullets
+    public float singleFireSpeed, pulseSpeed; //seconds between single fire bullets
     // Use this for initialization
     void Start () {
-        StartCoroutine(singleFireRoutine(singleFireSpeed));
+        //StartCoroutine(singleFireRoutine(singleFireSpeed, 10));
+        StartCoroutine(pulseRoutine(pulseSpeed, 2, 30));
     }
 	
 	// Update is called once per frame
@@ -15,9 +16,21 @@ public class BossFireManager : MonoBehaviour {
 		
 	}
 
-    public IEnumerator singleFireRoutine(float firerate) {
+    public IEnumerator singleFireRoutine(float firerate, float bulletspeed) {
+        standardBullet.projectileSpeed = bulletspeed;
         while (true) {
             Instantiate(standardBullet, transform.position, transform.rotation);
+            yield return new WaitForSeconds(firerate);
+        }
+    }
+
+    public IEnumerator pulseRoutine(float firerate, float bulletspeed, int pulseBullets) {
+        standardBullet.projectileSpeed = bulletspeed;
+        float angleInc = 360 / pulseBullets;
+        while (true) {
+            for(int i = 0; i < pulseBullets; i++) {
+                Instantiate(standardBullet, transform.position, Quaternion.AngleAxis(angleInc*i, Vector3.forward));
+            }
             yield return new WaitForSeconds(firerate);
         }
     }
