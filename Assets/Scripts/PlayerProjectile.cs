7025e230164655e4b48c828f080c25c8dbcd47cc
gadeuneo <8356171+gadeuneo@users.noreply.github.com>
2018-09-30 13:35:34 +0200

    Player update
    Shoot at mouse position improvement

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -11,16 +11,34 @@ public class PlayerProjectile : MonoBehaviour {
     // Lifetime of projectile
     public float time = 1.5f;
 
+    private Vector3 mousePosition;
+    private Vector2 directionVector;
+
     // Use this for initialization
     void Start () {
 
+        mousePosition = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        mousePosition.z = 0;
+        directionVector = (mousePosition - transform.position).normalized;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.position -= transform.position * projectileSpeed * Time.deltaTime;
+
+
+        //transform.position = transform.position + Camera.main.transform.forward * projectileSpeed * Time.deltaTime;
+        //set bomb at player pos
+
+        transform.Translate(directionVector * projectileSpeed * Time.deltaTime);
+
+
+        //transform.position = Vector2.MoveTowards(transform.position, mousePosition, projectileSpeed);
+        //goes to mouse position, then stops there
+
+
+        //transform.position += transform.position * projectileSpeed * Time.deltaTime;
         //Destroys projectile after t seconds
         Destroy(gameObject, time);
 	}
